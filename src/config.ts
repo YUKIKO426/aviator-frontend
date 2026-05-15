144d80ae3c99df23f4ebfe07ebfa148822c3b5a2
YUKIKO426 <nfker70@gmail.com>
2026-05-15 11:11:08 +0530

    Change config to disable development and debug modes

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
-  development: true,
-  debug: true,
+  development: false,
+  debug: false,
   appKey: "crash-0.1.0",
   api: "https://tarsha-staphyloplastic-capitally.ngrok-free.dev/api",
   wss: "https://tarsha-staphyloplastic-capitally.ngrok-free.dev",
