92b3fc98c327b1ed30fda19445212f92e4b29e42
YUKIKO426 <nfker70@gmail.com>
2026-05-15 11:07:31 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ export const config = {
   development: true,
   debug: true,
   appKey: "crash-0.1.0",
-  api: "http://localhost:5001/api",
-  wss: "http://localhost:5001",
+  api: "https://tarsha-staphyloplastic-capitally.ngrok-free.dev/api",
+  wss: "https://tarsha-staphyloplastic-capitally.ngrok-free.dev",
 };
